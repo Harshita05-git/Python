a1fecfcba3515d96ed3540a4e7a44c381e186499
Harshita05-git <harshita527.g@gmail.com>
2026-05-18 15:43:36 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,67 @@
+'''
+print("This is my first python program")
+What is python?
+------------------
+-->High-level language
+--No need to worry about low-level management
+------------------
+-->Dynamically typed language
+--No need to mention the kind of data taken in the variable
+------------------
+-->Interpreted language
+--Python will execute the code line-by-line and can throw the error if any but all other before lines will be executed and gives output.
+------------------
+
+num = 23
+any = [1,2]
+so = "Python"
+an = {"Name": "Teja"}
+print(num)
+print(any)
+print(so)
+print(an)
+-----------------
+Tokens
+-->Keywords in the program
+if
+else
+while
+for
+-----------------
+Identifiers
+-->Name of variables,functions,and classes
+
+variables
+-->can start with A-Z,a-z,_
+-->Cannot use special characters,spaces
+-->Cannot use 0-9 at the starting of the variable but can be used in between
+
+name = {} valid
+@97 = {} invalid
+0ab c = {] invalid
+r_h = {} valid
+-----------------
+HDFC_Viz_cus_details = {"Name":"Teja"}
+SBI_Viz_cus_details = {"Name":"Harsha"}
+------------------
+comments
+--------
+1.single-line(#)
+2.multi-line(''' ''')
+---------------------
+Boolean
+-------
+True or False
+
+num1 = 5
+num2 = 9
+print(num1 == num2)
+'''
+
+
+
+
+
+
+
+
